66bad0799af2f2ce56205675306324c42a532486
Thai Nguyen <59272463+ThaiNguyen54@users.noreply.github.com>
2022-09-21 16:37:03 +0700

    update my page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import MyAvatar from './MyAvatar.jpg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={MyAvatar} alt="My Avatar" width="500" height="500"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Welcome to my page
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href='https://restful-api-homework.herokuapp.com/message'>Click here to view my info</a>
+        <h2>This is my favorite song. Let's enjoy it</h2>
+         <iframe width="500" height="500" src="https://www.youtube.com/embed/ecBco63zvas">
+         </iframe>
       </header>
     </div>
   );
